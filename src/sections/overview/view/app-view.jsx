ee2df2d9463418c4094d2fc8fede76e29a1f6dc2
Javier <javierz87@gmail.com>
2024-08-16 10:50:31 -0400

    this is test1

--- src/sections/overview/view/app-view.jsx
+++ src/sections/overview/view/app-view.jsx
@@ -22,7 +22,7 @@ export default function AppView() {
   return (
     <Container maxWidth="xl">
       <Typography variant="h4" sx={{ mb: 5 }}>
-        Hi, Welcome back 👋
+        This is test1 👋
       </Typography>
 
       <Grid container spacing={3}>
